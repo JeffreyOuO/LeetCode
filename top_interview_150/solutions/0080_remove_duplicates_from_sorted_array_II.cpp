0ae40687a1d460f0f5b3d7850f917394fa3b0f96
Jeffrey <kartd0820@gmail.com>
2025-09-22 13:01:48 +0800

    0080

--- top_interview_150/solutions/0080_remove_duplicates_from_sorted_array_II.cpp
+++ top_interview_150/solutions/0080_remove_duplicates_from_sorted_array_II.cpp
@@ -20,11 +20,12 @@ public:
 
         while(readIdx<size){
             int duplicateCount=1;
+            // Count duplicates for current number
             while(readIdx+1<size&&nums[readIdx+1]==nums[readIdx]){
                 readIdx++;
                 duplicateCount++;
             }
-            
+            // Copy at most 2 occurrences
             for(int i=0;i<min(2,duplicateCount);i++){
                 nums[writeIdx]=nums[readIdx];
                 writeIdx++;
